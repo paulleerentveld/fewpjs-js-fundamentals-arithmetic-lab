260a0f88dbddaf27c6622dafa59ee68a97ccd781
paulleerentveld <paul@veldnet.com.au>
2022-03-04 10:23:25 +1000

    Add math variables for lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 10;
+let num4 = 6;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*10) +1;
+
+let mod = num3 % num4;
+
+let max = Math.max(1,2,3,4,5,6,12,14,20,16,18,19);
 
 // Write your code here
